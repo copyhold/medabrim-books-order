e6d6d77da910e9af28525611f4a9a1580fdb48f3
Ilya Nuevo <copyhold@outlook.com>
2023-06-02 12:06:53 +0300

    Init

---
+++ parse-message.js
@@ -0,0 +1,56 @@
+const html = `קיבלתם הודעה חדשה<br>Jesus life: true<br>New Testament: true<br>Supernatural: false<br>Isaiah 53: false<br>Lawyers Case: false<br>BeforeYou (CD): true<br>Name: ilya test 3<br>Street: הללויה<br>House Number: 99<br>Apartment Number: 87<br>City: אלום<br>Phone Number: 09876567<br>Age Confirmation: true<br><br>----------------------------------------------------------<br><br><br>\n' +
+    'If you believe this is a spam submission, please forward to <br><br><a href="https://webflow.com/dashboard/unsubscribeForm?t=9d1611c13a171d71632cc1a74a7c9f84c00fa695ae69f8c5b62cc127370aca2f&amp;s=5fa11abe42c4e96f4ed2984f&amp;sn=Medabrim&amp;e=zapier%40emailprocessing.site&amp;d=1685693874935">Unsubscribe</a> from notifications for this site.\n' +
+    '\n' +
+    '<br/><img src="https://x9x26.mjt.lu/oo/AMYAABl_xSUAAAAAAAAAARHR9jkAAAAAjncAAAAAABhTEABkeaW0AmffjtFsScKxq3Z10rbRfAAA7QM/d6ba69ad/e.gif" height="1" width="1" alt="" border="0" style="height:1px;width:1px;border:0;"/>\n`;
+const testmsg = `הודעה חדשה מאתר מדברים.
+
+קיבלתם הודעה חדשה
+Jesus life: true
+New Testament: false
+Supernatural: true
+Isaiah 53: false
+Lawyers Case: true
+BeforeYou (CD): false
+Name: מוריה
+Street: מעלה חסון
+House Number: 146
+Apartment Number: 16
+City: מעלות תרשיחא
+Phone Number: 0526545720
+Age Confirmation: true`;
+
+export const parseMessage = message => {
+  const lines = message.replaceAll(/<br>/g, "\n").split("\n")
+  const parsedMessage = {
+    books: [],
+    personalInformation: {}
+  };
+
+  for (let line of lines) {
+    const [key, value] = line.split(':');
+    if (!key || !value) continue
+    const trimmedKey = key.trim();
+    const trimmedValue = value.trim();
+
+    if (trimmedKey === 'Name') {
+      parsedMessage.personalInformation.name = trimmedValue;
+    } else if (trimmedKey === 'Street') {
+      parsedMessage.personalInformation.street = trimmedValue;
+    } else if (trimmedKey === 'House Number') {
+      parsedMessage.personalInformation.houseNumber = parseInt(trimmedValue);
+    } else if (trimmedKey === 'Apartment Number') {
+      parsedMessage.personalInformation.apartmentNumber = parseInt(trimmedValue);
+    } else if (trimmedKey === 'City') {
+      parsedMessage.personalInformation.city = trimmedValue;
+    } else if (trimmedKey === 'Phone Number') {
+      parsedMessage.personalInformation.phoneNumber = trimmedValue;
+    } else if (trimmedKey === 'Age Confirmation') {
+      parsedMessage.personalInformation.ageConfirmation = (trimmedValue.toLowerCase() === 'true');
+    } else if (trimmedValue.toLowerCase() === 'true') { // true after the book name
+      parsedMessage.books.push(trimmedKey)
+    }
+  }
+
+  return parsedMessage;
+};
+console.log(parseMessage(html))
---
+++ worker.js
@@ -0,0 +1,56 @@
+import { parseMessage } from './parse-message.js';
+const PostalMime = require('postal-mime/dist/node').postalMime.default;
+
+async function streamToArrayBuffer(stream, streamSize) {
+  let result = new Uint8Array(streamSize);
+  let bytesRead = 0;
+  const reader = stream.getReader();
+  while (true) {
+    const { done, value } = await reader.read();
+    if (done) {
+      break;
+    }
+    result.set(value, bytesRead);
+    bytesRead += value.length;
+  }
+  return result;
+}
+
+async function sendTGMessage(message, env) {
+  const token = env.TG_BOT_TOKEN;
+  const ids = env.TG_SEND_IDS.split(",");
+  const url = `https://api.telegram.org/bot${token}/sendMessage`;
+  const tgResponse = await fetch(url, {
+    method: "POST",
+    body: JSON.stringify({
+      chat_id: env.TG_SEND_IDS,
+      text: JSON.stringify(message),
+    }),
+    headers: {
+      "Content-Type": "application/json",
+    },
+  })
+  console.log(tgResponse.status, await tgResponse.text())
+}
+export default {
+  async email(message, env, ctx) {
+
+    if (message.raw) {
+      const rawEmail = await streamToArrayBuffer(message.raw, message.rawSize);
+      const parser = new PostalMime();
+      const parsedEmail = await parser.parse(rawEmail)
+
+      console.log(parsedEmail)
+
+      const decoder = new TextDecoder()
+      console.log(decoder.decode(rawEmail))
+
+      try {
+        const parsedMessage = parseMessage(parsedEmail.html)
+        await sendTGMessage(parsedMessage, env)
+      } catch (error) {
+        message.forward('novojilov.ilya@gmail.com')
+      }
+    }
+  },
+};
